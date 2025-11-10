c914dc2f4c0199d70afd83f8f47661c968808e76
RodrigoMartinsA <rodrigo.tins.a@hotmail.com>
2025-11-10 20:25:43 -0300

    Create nome.java

---
+++ exercicios/Ex001/nome.java
@@ -0,0 +1,6 @@
+public class nome {
+    public static void main (String[] args){
+        System.out.println("Olá Mundo!");
+        System.out.println("É assim que funciona para mandar o java escrever augo no terminal.");
+    }
+}
